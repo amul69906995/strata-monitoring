65b2201cd004380b3f4bd59ef863890a0e543f76
amul kumar <amul.kumar.min21@itbhu.ac.in>
2025-12-09 08:27:41 +0530

    fix file resolution error

--- client/src/components/PanelView.jsx
+++ client/src/components/PanelView.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import './PanelView.css';
-import PanelSnapshot from './Panelsnapshot';
+import PanelSnapshot from './PanelSnapshot';
 
 const PanelView = () => {
   const [panels, setPanels] = useState([]);
